9709955e1b08eb9397369b4a8ba49ba4b897a28d
ajaseem129 <jaseemabdul20@gmail.com>
2021-03-05 18:48:32 +0530

    Init

---
+++ app/src/main/java/com/example/tradebrainsassignment/Retrofit/ApiClient.kt
@@ -0,0 +1,4 @@
+package com.example.tradebrainsassignment.Retrofit
+
+class ApiClient {
+}
---
+++ app/src/main/java/com/example/tradebrainsassignment/Utils/Const.kt
@@ -0,0 +1,5 @@
+package com.example.tradebrainsassignment.Utils
+
+object Const {
+    val API_KEY="YD2YTSU2PS8KLWKH"
+}
